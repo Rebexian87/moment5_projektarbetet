a521d5f3533f9e3e8062f588cc204562fbc9f3bf
Rebecca Smith <rebecca.smith@hotmail.se>
2025-08-14 15:40:49 +0200

    Added cooment, removed unessissary comments and empty rows.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,63 +16,19 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent {
 
-
+  //Variables with types
   frontImage: string = "bild1.jpg"; 
   imageGrasFlowers: string = "blommor.jpg"; //https://pixabay.com/sv/photos/blomster%C3%A4ng-v%C3%A5r-natur-%C3%A4ng-n%C3%A4rbild-7955256/
-  imageStudent:string="bildStudent.png"
-  imageBooks:string="bildBooks.jpg"
+  imageStudent:string="bildStudent.png" //https://pixabay.com/sv/illustrations/sk%C3%B6nhet-sk%C3%B6n-kvinna-h%C3%A4lsa-l%C3%A4sning-9596371/
+  imageBooks:string="bildBooks.jpg" // https://pixabay.com/sv/illustrations/student-informationssystem-2317832/
+
+  //Constructor with RouterImport
    constructor(private router:Router) {}
 
+
+   //Function that navigates a user to a different site
    navigateTo() {
     this.router.navigate(['/courses'])
    }
-
- 
-   //https://pixabay.com/sv/illustrations/sk%C3%B6nhet-sk%C3%B6n-kvinna-h%C3%A4lsa-l%C3%A4sning-9596371/
-
-  // https://pixabay.com/sv/illustrations/student-informationssystem-2317832/
-
-  // courses: courses[] = [];
-
-  // filteredCourses: courses [] = [];
-
-  // filterValue: string='';
-
-  // sortedCourses: courses [] = [];
-
-
-
-  
-
-  // constructor(private CoursesService: CoursesService) {}
-
-  // ngOnInit(){
-  //     this.CoursesService.loadCourses().subscribe((courses)=> {this.courses=courses; this.filteredCourses=courses; })
-
-
-   
-  // }
-
-
-
-  // applyFilter(): void {
-  //  this.filteredCourses=this.courses.filter((course) => course.code.toLowerCase().includes(this.filterValue.toLowerCase()) || course.coursename.toLowerCase().includes(this.filterValue.toLowerCase()))
-      
-  // }
-
-  // sort1(): void{
-  //     this.sortedCourses=this.courses.sort((a, b) => a.code > b.code ? 1:-1)
-
-  // }
-
-  // sort2(): void{
-  //     this.sortedCourses=this.courses.sort((a, b) => a.coursename > b.coursename ? 1:-1)
-
-  // }
-
-  //  sort3(): void{
-  //     this.sortedCourses=this.courses.sort((a, b) => a.progression > b.progression ? 1:-1)
-
-  // }
 
 }
